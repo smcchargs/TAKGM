5b4804ab6988030aeb4ea7e4006b22feb3de4211
smcchargs <52867301+smcchargs@users.noreply.github.com>
2021-03-31 15:27:54 -0400

    Create Test World
    Commit Test

---
+++ Source/TAKGM/TestPawn.h
@@ -0,0 +1,43 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Pawn.h"
+#include "TestPawn.generated.h"
+
+UCLASS()
+class TAKGM_API ATestPawn : public APawn
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this pawn's properties
+	ATestPawn();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UPROPERTY(EditAnywhere)
+		UStaticMeshComponent* MeshComponent;
+
+	UPROPERTY(EditAnywhere)
+		class UCameraComponent* Camera;
+
+	UPROPERTY(EditAnywhere, Category = "Pawn Movement")
+		float MaxSpeed;
+
+private:
+	void MoveForward(float value);
+	void MoveRight(float value);
+
+	FVector CurrentVelocity;
+};
